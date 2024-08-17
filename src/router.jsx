b8f59e55cfc449ddeafceb3219a51b84f9fdd1f5
Bayastan <bsbaha195@mail.com>
2024-08-17 14:45:36 +0600

    added router

---
+++ src/router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
