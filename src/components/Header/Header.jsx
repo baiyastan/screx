ffc57214aa4d0b60aa7a562ee6f2deae873e280d
Akbeike <akbijkekuttubekovna@gmail.com>
2024-08-19 11:24:28 +0600

    Added logo(1).svg folder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import "./header.css";
+import logo from"../../assets/logo(1).svg"
+
 
 function Header() {
   return <div>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
+
+    <section className="header">
+      <div className="logo">
+        <img src={logo} alt="" />
+        <h1>Scorex</h1>
+      </div>
+
+      <div className="menu">
+        <ul>
+          <li>Home</li>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>About Us</li>
+          <li>FAQ</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div className="register">
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </section>
+    
+   
+
+
   </div>;
 }
 
